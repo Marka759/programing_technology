0a45a09904a21b96f53590834e38264c3ab29350
Marka759 <minecrarina@mail.ru>
2026-09-25 15:13:49 +0700

    #1 Тест

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -6,7 +6,7 @@
         {
          BankAccount account1 = new BankAccount("Yana", 1000000);
          BankAccount account2 = new BankAccount("Lena", 100);
-            Console.WriteLine($"{account1.Owner} {account1.Balance} {account1.Number}");
+            Console.WriteLine($"{account1.Owner} { account1.Balance} {account1.Number}");
             Console.WriteLine($"{account2.Owner} {account2.Balance} {account2.Number}");
             account1.MakeDeposite(12000, DateTime.UtcNow, ";)");
             Console.WriteLine($"Balance: {account1.Balance}");
